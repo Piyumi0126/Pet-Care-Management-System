071744db2ff84efb1e9a45e271ec059c8d14a48f
Piyumi0126 <piyuhimasha6032@gmail.com>
2024-05-04 17:17:05 +0530

    Configuration file added

---
+++ app.js
@@ -0,0 +1,37 @@
+/*const express = require('express');
+const app = express();
+const PORT = 8801;
+
+app.listen(PORT, () => {
+  console.log("Server is running on port ${PORT}");
+});
+*/
+import express from "express";
+import{config} from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app=express();
+config({path : "./config/config.env"});
+
+app.use(cors({
+  origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+  method: ["GET", "POST", "DELETE", "PUT"],
+  credentials: true,
+})
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp",
+  })
+);
+
+
+export default app;
